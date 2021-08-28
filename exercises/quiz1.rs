d2332f6d03b9f68c05b0e75c2a476f37c7b13b0f
Daigo Shitara <stain.witness+github@gmail.com>
2021-08-28 12:58:45 +0900

    import

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -5,21 +5,27 @@
 
 // Mary is buying apples. One apple usually costs 2 Rustbucks, but if you buy
 // more than 40 at once, each apple only costs 1! Write a function that calculates
-// the price of an order of apples given the quantity bought. No hints this time!
-
-// I AM NOT DONE
+// the price of an order of apples given the order amount. No hints this time!
 
 // Put your function here!
 // fn ..... {
+const COST_NORMAL: i32 = 2;
+const COST_DISCOUNT: i32 = 1;
+
+fn calculate_apple_price(n: i32) -> i32 {
+    if (n >= 40) {
+        n * COST_DISCOUNT
+    } else {
+        n * COST_NORMAL
+    }
+}
 
 // Don't modify this function!
 #[test]
 fn verify_test() {
     let price1 = calculate_apple_price(35);
-    let price2 = calculate_apple_price(40);
-    let price3 = calculate_apple_price(65);
+    let price2 = calculate_apple_price(65);
 
     assert_eq!(70, price1);
-    assert_eq!(80, price2);
-    assert_eq!(65, price3);
+    assert_eq!(65, price2);
 }
--- exercises/standard_library_types/iterators5.rs
+++ exercises/standard_library_types/iterators5.rs
@@ -1,4 +1,5 @@
 // iterators5.rs
+
 // Let's define a simple model to track Rustlings exercise progress. Progress
 // will be modelled using a hash map. The name of the exercise is the key and
 // the progress is the value. Two counting functions were created to count the
@@ -6,7 +7,8 @@
 // imperative style for loops. Recreate this counting functionality using
 // iterators. Only the two iterator methods (count_iterator and
 // count_collection_iterator) need to be modified.
-// Execute `rustlings hint iterators5` for hints.
+// Execute `rustlings hint
+// iterators5` for hints.
 //
 // Make the code compile and the tests pass.
 
@@ -14,7 +16,7 @@
 
 use std::collections::HashMap;
 
-#[derive(Clone, Copy, PartialEq, Eq)]
+#[derive(PartialEq, Eq)]
 enum Progress {
     None,
     Some,
